22b30d556b0944b830032be119087b4fe35dd483
Tornikeramazashvili <mnateus@gmail.com>
2024-01-12 23:37:25 +0400

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-between">
+        <span>Hello</span>
+        <span>zaza</span>
+      </div>
+    </>
+  );
+}
+
+export default App;
